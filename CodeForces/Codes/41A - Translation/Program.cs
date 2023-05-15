4f372ff9a006416b1525db7f3753d31f9c5eb67d
MD SHAKIBUL HOSSAIN SIKDER <SIKDER.SWE@GMAIL.COM>
2023-05-15 18:26:17 +0600

    Solved code

--- CodeForces/Codes/41A - Translation/Program.cs
+++ CodeForces/Codes/41A - Translation/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Problem link : https://codeforces.com/problemset/problem/41/a
+
+// Submission link : https://codeforces.com/contest/41/submission/205979898
+using System;
+using static System.Console;
+
+string BerlandWord, BirlandWord;
+
+BerlandWord = ReadLine();
+BirlandWord= ReadLine();
+
+string blank= null;
+
+for(int i = BirlandWord.Length-1; i>= 0 ; i--)
+{
+    blank += BirlandWord[i];
+}
+
+if (blank == BerlandWord)
+    Write("YES");
+else
+    Write("NO");
