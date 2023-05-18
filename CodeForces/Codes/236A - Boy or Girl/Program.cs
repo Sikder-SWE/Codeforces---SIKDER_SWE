7ec1f820e0ae64452800c54392c161a317644064
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-05-18 22:08:08 +0600

    Solved Problem 236A

---
+++ CodeForces/Codes/236A - Boy or Girl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+class Program236A
+{
+    static void Main()
+    {
+        string userName;
+        //string tmp;
+        int distinctChar=0;
+        userName = Console.ReadLine();
+        HashSet<char> tmp = new HashSet<char>();
+        for(int i=0; i<userName.Length; i++)
+        {
+            tmp.Add(userName[i]);
+        }
+        userName = null;
+        foreach(char ch in tmp)
+        userName+=ch;
+        //Write(userName);
+
+        if (userName.Length % 2 == 0)
+            Write("CHAT WITH HER!");
+        else
+            Write("IGNORE HIM!");
+    }
+}
