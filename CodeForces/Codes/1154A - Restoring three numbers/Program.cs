962e61ce0897f181ecf6444f9d2cf74de7ed6980
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-05-21 19:52:53 +0600

    Solved Problem 1154A - Restoring three numbers

---
+++ CodeForces/Codes/1154A - Restoring three numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+class Problem1154A
+{
+    int[] Restore(int[] Numbers)
+    {
+        Array.Sort(Numbers);
+        int Sum_abc = Numbers[Numbers.Length-1];
+        //WriteLine(Sum_abc);
+        for (int i = 0; i < Numbers.Length - 1; i++)
+        {
+            Numbers[i] = Sum_abc - Numbers[i];
+            //Write($"{Numbers[i]} ");  //Debugged
+        }
+        return Numbers;
+    }
+    static void Main()
+    {
+        int[] Integers = Array.ConvertAll(ReadLine().Split(), int.Parse);
+
+        Problem1154A solution = new Problem1154A();
+        int[] Numbers = solution.Restore(Integers);
+        Write($"{Numbers[0]} {Numbers[1]} {Numbers[2]}");
+    }
+}
