6b6a3878979a854507ea476f86af3f602b0529e3
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-05-22 21:54:04 +0600

    Solved Problem 1829A - Love Strory (Contest)

---
+++ CodeForces/Codes/1829A - Love Strory (Contest)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+public class Problem_Contest1829A
+{
+    public static void Main()
+    {
+
+        string fixedWord = "codeforces";
+        int testCase = int.Parse(ReadLine());
+        while (testCase > 0)
+        {
+            testCase--;
+            string word = ReadLine();
+            int count = 0;
+
+            for (int i = 0; i < fixedWord.Length; i++)
+            {
+                if (fixedWord[i] != word[i])
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
