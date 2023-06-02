0c0bce096e938d00da8f3a933d413fc4fa69adf8
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-02 22:31:35 +0600

    Solved 344A - Magnets

---
+++ CodeForces/Codes/344A - Magnets/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+class Problem_344A
+{
+    public int Solution(int TestCase, string[] Arrangement)
+    {
+
+        int Group = 1;
+
+
+        for (int i = 0; i < TestCase - 1; i++)
+        {
+            bool connection = false;
+            if (Arrangement[i] == Arrangement[i + 1])
+                connection = true;
+
+            if (connection)
+                continue;
+            else
+                Group++;
+        }
+        return Group;
+
+        /*foreach(string indexValue in Arrangement)
+        Write($" {indexValue}");*/
+    }
+
+    static void Main()
+    {
+        int TestCase = int.Parse(ReadLine());
+        string[] Poles = new string[TestCase];
+
+        for (int i = 0; i < TestCase; i++)
+            Poles[i] = ReadLine();
+
+        Problem_344A code = new Problem_344A();
+
+        //code.Solution(TestCase, Poles);
+
+
+        //int Answer = code.Solution(TestCase, Poles);
+        Write($"{code.Solution(TestCase, Poles)}");
+
+
+    }
+}
