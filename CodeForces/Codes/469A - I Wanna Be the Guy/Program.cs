78b366287e35a5c6c511193ec7dbf028d7b09b7d
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-09 12:13:39 +0600

    Solved in C#

---
+++ CodeForces/Codes/469A - I Wanna Be the Guy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+
+class Problem_469A
+{
+    public string Solve(int totalLevels, int[] Xpasses, int[] LxPasses)
+    {
+
+        /*int totalLevels = int.Parse(Console.ReadLine());
+
+        int[] Xpasses = Array.ConvertAll(ReadLine().Split(), int.Parse);
+        int[] LxPasses = Array.ConvertAll(ReadLine().Split(), int.Parse);
+*/
+
+        List<int> passedLevels = new List<int>();
+
+        for (int i = 1; i < Xpasses.Length; i++)
+            passedLevels.Add(Xpasses[i]);
+
+        for (int i = 1; i < LxPasses.Length; i++)
+            passedLevels.Add(LxPasses[i]);
+
+        int[] passed = passedLevels.Distinct().ToArray();
+
+        //Array.Sort(passed);
+
+        //passedLevels.Distinct().ToList();
+
+        if (passed.Length != totalLevels)
+            return "Oh, my keyboard!";
+        else
+            return "I become the guy.";
+    }
+
+    static void Main()
+    {
+        int totalLevels = int.Parse(Console.ReadLine());
+
+        int[] Xpasses = Array.ConvertAll(ReadLine().Split(), int.Parse);
+        int[] LxPasses = Array.ConvertAll(ReadLine().Split(), int.Parse);
+
+        Problem_469A solution = new Problem_469A();
+
+        Write(solution.Solve(totalLevels, Xpasses, LxPasses));
+    }
+}
