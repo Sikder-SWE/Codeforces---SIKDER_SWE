4ccce4a482428310abddde20a6b964aeecc61f17
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 16:54:42 +0600

    Solved

---
+++ CodeForces/Codes/1560A - Dislike of Threes/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+class Problem_1560A
+{
+    static void Main()
+    {
+        int[] NumbersNotDevided = new int[1800];
+        int Initial = 1;
+        int testCase = int.Parse(ReadLine());
+        for(int i = 0; i < 1800;)
+        {
+            if(Initial%3!= 0 && Initial%10!=3)
+            {                
+                NumbersNotDevided[i] = Initial;               
+                Initial++;
+                i++;
+                continue;
+            }
+            else
+            {
+                Initial++;
+                continue;
+            }
+        }
+
+        while(testCase>0)
+        {
+            testCase--;
+            WriteLine(NumbersNotDevided[int.Parse(ReadLine())-1]);
+        }
+    }
+}
