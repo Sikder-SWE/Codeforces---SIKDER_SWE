6d5dbcc02780b7ad14ce5e141f587510b500e886
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 12:10:57 +0600

    Solved

---
+++ CodeForces/Codes/4A - Watermelon/Program.cs
@@ -0,0 +1,29 @@
+﻿// problem link : https://codeforces.com/contest/4/problem/A
+using System;
+
+class Problem_4A
+{
+    static void Main()
+    {
+        int weight;
+        weight = int.Parse(Console.ReadLine());
+        if (weight > 2)
+        {
+            if (weight % 2 == 0)
+            {
+                if ((weight / 2) % 2 == 0 || weight / 2 != 1)
+                {
+                    Console.Write("YES");
+                }
+
+            }
+            else
+                Console.Write("NO");
+
+        }
+        else
+            Console.Write("NO");
+
+    }
+
+}
