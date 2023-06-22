80075f6c389c6452976fdaf0c12e5df2e80df69b
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 12:19:03 +0600

    Solved

---
+++ CodeForces/Codes/116A - Tram/Program.cs
@@ -0,0 +1,25 @@
+﻿// Problem Link : https://codeforces.com/problemset/problem/116/A
+
+using System;
+using static System.Console;
+
+public class Program
+{
+    static void Main()
+    {
+        int Test_stopage = int.Parse(ReadLine());
+
+        int Rp = 0, Ep, Lp = 0, Capacity = 0;
+        while (Test_stopage > 0)
+        {
+            Test_stopage--;
+            int[] Passenger = Array.ConvertAll(ReadLine().Split(' '), int.Parse);
+
+            Ep = Passenger[0]; Lp = Passenger[1];
+            Rp = Rp + (Lp - Ep);
+
+            Capacity = Capacity > Rp ? Capacity : Rp;
+        }
+        WriteLine(Capacity);
+    }
+}
