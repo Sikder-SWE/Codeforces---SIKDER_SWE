233f9eca22077ad3076225ad57dc1bdbda06c61f
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 12:15:47 +0600

    Solved

---
+++ CodeForces/Codes/59A - Word/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+class Problem_59A
+{
+     public int Upper = 0, Lower = 0;
+     
+    public void Solution(string Word)
+    {
+        foreach (var k in Word)
+            if ((int)k > 96) Lower++;
+            else
+                Upper++;
+    }
+    
+    static void Main()
+    {
+        Problem_59A solve = new Problem_59A();
+        string Word = ReadLine();
+        solve.Solution(Word);
+        Write(solve.Lower >= solve.Upper ? Word.ToLower() : Word.ToUpper());
+    }
+}
