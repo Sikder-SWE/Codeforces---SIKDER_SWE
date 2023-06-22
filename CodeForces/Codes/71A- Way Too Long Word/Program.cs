ce3963334422881a4dc052903c9389b858a1208c
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 12:13:13 +0600

    Solved

---
+++ CodeForces/Codes/71A- Way Too Long Word/Program.cs
@@ -0,0 +1,24 @@
+﻿//Problem link: https://codeforces.com/contest/71/problem/A
+
+using System;
+
+class Problem_71A
+{
+    static void Main()
+    {
+        string word;
+        int test_case;
+        test_case = int.Parse(Console.ReadLine());
+        while (test_case != 0)
+        {
+            word = Console.ReadLine();
+            if (word.Length > 10)
+            {
+                Console.WriteLine($"{word[0]}{word.Length - 2}{word[word.Length - 1]}");
+            }
+            else
+                Console.WriteLine($"{word}");
+            test_case--;
+        }
+    }
+}
