3a3a8e4ac7cbbd7ced36c2bd8a81384dd465e6ec
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 18:22:12 +0600

    Solved

---
+++ CodeForces/Codes/318A - Even Odds/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Problem_318A
+{
+    static void Main()
+    {
+        long[] input = Array.ConvertAll(ReadLine().Split(), long.Parse);
+
+        long middle = (input[0] + 1) /2;
+
+        if (input[1]>middle)
+        {
+            Write(2 * (input[1] - middle));
+        }
+        else
+        {
+            Write((2 * input[1]) -1);
+        }
+    }
+ }
