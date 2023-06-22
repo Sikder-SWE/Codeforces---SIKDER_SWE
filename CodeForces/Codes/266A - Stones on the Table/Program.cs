f983747650a1156315306ea2c3b0f55336ddd0bd
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-22 12:17:37 +0600

    Solved

---
+++ CodeForces/Codes/266A - Stones on the Table/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+class Problem_266A
+{
+    static void Main()
+    {
+        int TotalStones = int.Parse(ReadLine());    // I will store how many stones will be given
+        string stones = ReadLine();                 // It will store color string to check for the program
+        int RemovedStones = 0;                      // this variable will calculate how many stones can be removed form the string
+                                                    //	WriteLine(stones);
+        if (TotalStones > 1)
+        {
+            for (int i = 0; i <= (stones.Length) - 2; i++)
+            {
+                if (stones[i] == stones[i + 1])     // this is accroding to program statement:: logic
+                {
+                    RemovedStones++;
+                    continue;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            WriteLine(RemovedStones);
+        }
+        else
+        {
+            WriteLine(0);
+        }
+    }
+}
