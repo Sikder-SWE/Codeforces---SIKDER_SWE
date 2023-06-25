522400ed47d2c976f7714b690573edab54d7e691
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-25 17:50:21 +0600

    Solved

---
+++ CodeForces/Codes/520A - PANGRAM/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+class Problem_520A
+{
+    static void Main()
+    {
+        _ = ReadLine();
+        char[] word = ReadLine().ToLower().Distinct().ToArray();
+        WriteLine(word.Length == 26 ? "YES" : "NO");
+    }
+}
