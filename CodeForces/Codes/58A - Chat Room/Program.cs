d26897a7902aa6dc96823086ffd07914dd88d026
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-06-26 18:40:29 +0600

    Solved

---
+++ CodeForces/Codes/58A - Chat Room/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Problem58A
+{
+    public static void Main(string[] args)
+    {
+        string MainWord = "hello";
+
+        string Word = ReadLine();
+        int count = 0;
+        for (int i = 0;i<Word.Length;i++)
+        {
+            if (MainWord[count] == Word[i])
+                count++;
+
+            if (count == 5) break;
+        }
+        Write(count==5?"YES":"NO");
+    }
+}
