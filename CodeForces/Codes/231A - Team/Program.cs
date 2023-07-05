9a81acc3dbb874dc8415468af038161e7a57430b
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:49:46 +0600

    solved

---
+++ CodeForces/Codes/231A - Team/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Problem_231A
+{
+    static void Main()
+    {
+        int totalAssessment = 0;
+
+        int numOfTasks = int.Parse(ReadLine());
+        for (int i = 0; i < numOfTasks; i++)
+        {
+            string? tasks = ReadLine();
+            if (tasks.Split('1').Length - 1 >= 2)
+                totalAssessment++;
+
+        }
+
+        WriteLine(totalAssessment);
+    }
+}
