aa2f1139f3421c74c933e8faa4b60c9d9601a6cb
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:50:06 +0600

    solved

---
+++ CodeForces/Codes/9A - Die Roll/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+class Problem_9A
+{
+    static void Main()
+    {
+        int[] Values = Array.ConvertAll(ReadLine().Split(), int.Parse);
+        int res = 7 - Math.Max(Values[0], Values[1]);
+        switch (res)
+        {
+            case 2:
+            case 4:
+                {
+                    WriteLine(res / 2 + "/3");
+                    break;
+                }
+            case 6:
+                {
+                    WriteLine("1/1");
+                    break;
+                }
+            case 3:
+                {
+                    WriteLine("1/2");
+                    break;
+                }
+            case 5:
+                {
+                    WriteLine("5/6");
+                    break;
+                }
+            case 1:
+                {
+                    WriteLine(res + "/6");
+                    break;
+                }
+        }
+    }
+}
