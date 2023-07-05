1c9a7dc94943b7538af4c6cfae0e710e0ce3c4a8
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:10:55 +0600

    All

---
+++ CodeForces/Codes/271A - Beautiful Year/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Problem_271A
+{
+    static void Main()
+    {
+        int y = Convert.ToInt32(Console.ReadLine());
+        while (true)
+        {
+            y += 1;
+            int a = y / 1000;
+            int b = y / 100 % 10;
+            int c = y / 10 % 10;
+            int d = y % 10;
+            if (a != b && a != c && a != d && b != c && b != d && c != d)
+            {
+                break;
+            }
+        }
+        Console.WriteLine(y);
+    }
+}
