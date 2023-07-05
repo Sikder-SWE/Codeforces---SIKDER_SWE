e8b8a06df5dafd40e11173f10483f0669bc30c7a
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:50:30 +0600

    solved

---
+++ CodeForces/Codes/160A - Twins/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+class Problem_160A
+{
+    static void Main()
+    {
+        int tatal = int.Parse(ReadLine());
+        int[] coins = ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Array.Sort(coins);
+        Array.Reverse(coins);
+        int summation = 0, maxCoin = 0, MaxV = 0;
+        for (int i = 0; i < tatal; i++)
+        {
+            summation += coins[i];
+        }
+        for (int i = 0; i < tatal; i++)
+        {
+            if ((summation / 2) >= MaxV)
+            {
+                MaxV += coins[i];
+                maxCoin++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        WriteLine(maxCoin);
+    }
+}
