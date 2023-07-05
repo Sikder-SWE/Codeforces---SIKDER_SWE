d1f78e56ee1784dd2a1f93f07ff19e17bb96221e
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:11:52 +0600

    Solved

---
+++ CodeForces/Codes/723A - The new Year - Meeting friends/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Problem_723A
+{
+    public static void Main()
+    {
+        List<int> distance = new List<int> { };
+        var data = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            int x = Convert.ToInt32(data[i]);
+            distance.Add(x);
+        }
+        distance.Sort();
+        Console.WriteLine((distance[2] - distance[1]) + (distance[1] - distance[0]));
+    }
+}
