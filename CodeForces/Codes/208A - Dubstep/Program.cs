10867dc28723b158c5d2f1caaca2a53c16c9add6
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:10:36 +0600

    Solved

---
+++ CodeForces/Codes/208A - Dubstep/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gaber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+            string backup = "", song = "";
+            char[] chars = { 'W', 'U', 'B' };
+            int current = 0;
+
+            words = words.Replace("WUB", " ");
+            words = words.Trim();
+
+            while (words.Contains("  "))
+            {
+                words = words.Replace("  ", " ");
+            }
+
+            Console.Write(words);
+        }
+    }
+}
