af356198584c6ae93a136667f38c59969ddad707
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-05 19:11:21 +0600

    Solved

---
+++ CodeForces/Codes/581A - Vasua the Hipster/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+class Problem_581A
+{
+    public static void Main(string[] args)
+    {
+        var inputs = Console.ReadLine().Split(' ');
+        int a = int.Parse(inputs[0]);
+        int b = int.Parse(inputs[1]);
+
+        int fashion = Math.Min(a, b);
+        int max = Math.Max(a, b);
+
+        Console.WriteLine(fashion + " " + (max - fashion) / 2);
+    }
+}
