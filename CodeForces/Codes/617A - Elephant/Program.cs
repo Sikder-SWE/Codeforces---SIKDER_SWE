93244537d3797dcaf9f751848096faed7ba2ad8a
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-06 06:43:41 +0600

    solved

---
+++ CodeForces/Codes/617A - Elephant/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+class Problem_617A
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(ReadLine());
+        if (x % 5 == 0)
+            WriteLine(x / 5);
+        else
+            WriteLine((x / 5) + 1);
+    }
+}
