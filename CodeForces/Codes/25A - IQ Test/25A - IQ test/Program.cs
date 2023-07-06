9d70c0d3ce350a5a9a080a08237d9066f914787b
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-06 06:41:43 +0600

    solved

---
+++ CodeForces/Codes/25A - IQ Test/25A - IQ test/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+class Problem_25A
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(ReadLine());
+string[] numbers = ReadLine().Split(' ');
+int even = 0;
+int odd = 0;
+int EvenPosition = 1;
+int OddPosition = 0;
+for (int i = 0; i < n; i++)
+{
+    if (int.Parse(numbers[i]) % 2 == 0)
+    {
+        even++;
+        EvenPosition = i + 1;
+    }
+    else
+    {
+        odd++;
+        OddPosition = i + 1;
+    }
+}
+if(odd == 1)
+{
+    Write(OddPosition);
+}
+else if (even == 1)
+{
+    WriteLine(EvenPosition);
+}
+
+    }
+}
