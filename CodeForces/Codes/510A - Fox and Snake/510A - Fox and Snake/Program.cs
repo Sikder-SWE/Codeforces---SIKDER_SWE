41bd7a59e37df8afdca421c005a4e9fafaa6c000
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-06 06:43:18 +0600

    solved

---
+++ CodeForces/Codes/510A - Fox and Snake/510A - Fox and Snake/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+class  Problem_510A
+{
+    static void Main(string[] args)
+    {
+        string s = ReadLine();
+string[] values = s.Split(' ');
+
+int N = int.Parse(values[0]);
+int M = int.Parse(values[1]);
+
+for(int i = 1; i <= N; i++)
+{
+    if (i % 2 != 0)
+    {
+        for (int j = 1; j <= M; j++)
+        {
+            Write('#');
+        }
+        WriteLine();
+    }
+    else
+    {
+        if(i % 4 != 0)
+        {
+            for (int j = 1; j < M; j++)
+            {
+                Write('.');
+            }
+            Write('#');
+            WriteLine();
+        }
+        else
+        {
+            Write('#');
+            for (int j = 2; j <=M; j++)
+            {
+                Write('.');
+            }
+           
+            WriteLine();
+        }
+    }   
+}
+    }
+}
