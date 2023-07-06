6094158c04b2194d1ffa3e44dc26260169ada6ef
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-06 06:42:58 +0600

    solved

---
+++ CodeForces/Codes/281A - Word Capitalization/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+class Problem_281A
+{
+    static void Main(string[] args)
+    {
+        var x = ReadLine();
+        var y = x.Substring(0, 1).ToUpper() + x.Substring(1);
+        WriteLine(y);
+    }
+}
