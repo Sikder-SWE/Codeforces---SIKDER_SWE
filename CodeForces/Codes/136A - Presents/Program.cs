c7d300763ffa8d993412a56012c3f95bad277fc8
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-06 06:42:07 +0600

    solved

---
+++ CodeForces/Codes/136A - Presents/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+class Problem_136A
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(ReadLine());
+        int[] q = new int[n];
+
+        string nums = ReadLine();
+        string[] num = nums.Split(" ");
+        int[] p = Array.ConvertAll(num, Convert.ToInt32);
+
+        for (int i = 1; i <= n; i++)
+        {
+            q[p[i - 1] - 1] = i;
+        }
+
+        WriteLine(String.Join(" ", q));
+    }
+}
