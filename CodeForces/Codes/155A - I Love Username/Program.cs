3d88ce23977375a26dc5fd70d94aced5ef5ba8b0
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-06 06:42:34 +0600

    solved

---
+++ CodeForces/Codes/155A - I Love Username/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+class Problem_155A
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(ReadLine());
+        string str = ReadLine();
+        string[] values = str.Split(" ");
+        int[] num = Array.ConvertAll(values, Convert.ToInt32);
+
+        int[] Array1 = new int[0];
+        int count = 0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            Array1 = Array1.Concat(new int[] { num[i] }).ToArray();
+            if (num[i + 1] > Array1.Max()) count++;
+            if (num[i + 1] < Array1.Min()) count++;
+        }
+        WriteLine(count);
+    }
+}
