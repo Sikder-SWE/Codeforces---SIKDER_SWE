840a3fc47044fe3b07c656ed379c387223c52fd6
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 11:04:56 +0600

    solved

---
+++ CodeForces/Codes/1360A - Minimal Square/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+public class Program
+{
+    public static void Main()
+    {
+        long test, a, b;
+        test=long.Parse(ReadLine());
+        while(test>0)
+        {
+            string[] input = ReadLine().Split(' ');
+            a = long.Parse(input[0]);
+            b = long.Parse(input[1]);
+            if(a > b)
+            {
+               long temp = a; 
+                a = b; 
+                b = temp;
+            }
+            long result= Math.Max(a*2, b);
+            WriteLine(result*result);
+            test--;
+        }
+
+    }
+}
