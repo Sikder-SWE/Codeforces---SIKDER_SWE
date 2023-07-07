2dffe3bed92644cc691675cd975e071d2a3ce3f0
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 23:01:28 +0600

    solved

---
+++ CodeForces/Codes/432A - Choosing Team/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+class Problem_432A
+{
+    public static void Main(string[] args)
+    {
+        string[] nums = ReadLine().Split(' ');
+int n = int.Parse(nums[0]);
+int k = int.Parse(nums[1]);
+int[] store = new int[n];
+string[] inputs = ReadLine().Split(' ');
+for (int i = 0; i <n; i++)
+{
+    store[i] = int.Parse(inputs[i]);
+}
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (store[i] + k <= 5)
+    {
+        count++;
+    }
+}
+WriteLine(count / 3);
+    }
+}
