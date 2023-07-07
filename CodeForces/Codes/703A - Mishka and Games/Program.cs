0851dea86987b9305d98d485ca5f2ef9a45becec
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 23:48:49 +0600

    Solved

---
+++ CodeForces/Codes/703A - Mishka and Games/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+class Problem_703A
+{
+    public static void Main(string[] args)
+    {
+        int x;
+var variable = ReadLine();
+x=int.Parse(variable);
+int Mishka = 0, Player2 = 0;
+for (int i = 0; i < x; i++)
+{
+    var w = ReadLine();
+    var e = w.Split(" ");
+    if (int.Parse(e[0]) > int.Parse(e[1]))
+        Mishka++;
+    else if (int.Parse(e[0]) < int.Parse(e[1]))
+        Player2++;
+}
+
+if (Mishka > Player2)
+    WriteLine("Mishka");
+else if (Mishka == Player2)
+    WriteLine("Friendship is magic!^^");
+else if (Mishka < Player2)
+    WriteLine("Chris");
+
+    }
+}
