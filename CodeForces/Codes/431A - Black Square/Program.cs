fcaccce12cfec378ac9cfb5584a31b70f2566333
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 11:04:27 +0600

    solved

---
+++ CodeForces/Codes/431A - Black Square/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+class Problem_431A
+{
+    static void Main()
+    {
+        string[] s = ReadLine().Split(' ');
+        int[]x = new int[4];
+        for (int i = 0; i < 4; i++) x[i] = int.Parse(s[i]);
+        string s2 = ReadLine();
+        int count = 0;
+        for (int i = 0; i < s2.Length; i++)
+        {
+            switch (s2[i])
+            {
+                case '1':
+                    count +=x[0];
+                    break;
+                case '2':
+                    count += x[1];
+                    break;
+                case '3':
+                    count += x[2];
+                    break;
+                case '4':
+                    count += x[3];
+                    break;
+                default:
+                    break;
+            }
+        }
+        WriteLine(count);        
+    }
+}
+
