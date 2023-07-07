ba1e0ee604a36fb9dfa4142709a2bd1b1600a361
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 23:48:34 +0600

    solved

---
+++ CodeForces/Codes/379A - New Year Candles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+ class Problem_379A
+{
+	public static void Main()
+	{
+		string Value = ReadLine();
+		int[] temp = Value.Split(' ').Select(int.Parse).ToArray();
+		int res = temp[0];
+		int tem = 0;
+		
+		while(temp[0] >= temp[1]){
+			tem = temp[0] % temp[1];
+			temp[0] /=  temp[1];
+			res += temp[0];
+			temp[0] += tem;
+		}
+		res += tem / temp[1];
+		Write(res);
+	}
+}
