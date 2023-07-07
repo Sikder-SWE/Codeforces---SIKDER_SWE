6af4ff24fffe70a5f960ec2cf7fd425da4a78c7d
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 11:05:25 +0600

    solved

---
+++ CodeForces/Codes/1475A - Odd Divisor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+    class Problem_1475A
+    {
+        static void Main(string[] args)
+        {
+            string input = ReadLine();
+            int x= Convert.ToInt32(input);
+            for (int i = 0; i < x; i++)
+            {
+                long y = Convert.ToInt64(ReadLine());
+                while (y % 2 == 0)
+                {
+                    y = y / 2;
+                }
+                if (y == 1)
+                {
+                    WriteLine("NO");
+                }
+                else
+                {
+                    WriteLine("YES");
+                }
+            }           
+        }
+    }
