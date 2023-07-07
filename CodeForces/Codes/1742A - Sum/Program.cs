c58ea4f24f0cc595f2e18c797c91d4aa58d8c0e3
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 23:04:31 +0600

    solved

---
+++ CodeForces/Codes/1742A - Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+class Problem_1742A
+{
+    public static void Main()
+    {
+        var length = int.Parse(ReadLine()!);
+for (int i = 0; i != length; i++)
+{
+    var number = ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+    //Console.WriteLine(number);
+    if (number[1] + number[2] == number[0])
+        WriteLine("YES");
+
+    else if (number[2] + number[0] == number[1])
+        WriteLine("YES");
+
+    else if (number[0] + number[1] == number[2])
+        WriteLine("YES");
+    else
+        WriteLine("NO");
+}
+
+    }
+}
