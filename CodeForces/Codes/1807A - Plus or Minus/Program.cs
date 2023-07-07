a0497a175a78a6fca756c93a85a15454d5e5cfc2
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 23:49:11 +0600

    Solved

---
+++ CodeForces/Codes/1807A - Plus or Minus/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+class Problem_1807A
+{
+    public static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(ReadLine());
+
+for(int i=0; i<t; i++)
+{
+    string[] values = ReadLine().Split(' ');
+
+    int a = Convert.ToInt32(values[0]);
+    int b = Convert.ToInt32(values[1]);
+    int c = Convert.ToInt32(values[2]);
+
+    if (a + b == c) WriteLine('+');
+    else WriteLine('-');
+}
+    }
+}
