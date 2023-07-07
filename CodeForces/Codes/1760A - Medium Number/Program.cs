a54da6d020499bc21b6142a044bbe038a9ddcf1b
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 11:05:52 +0600

    solved

---
+++ CodeForces/Codes/1760A - Medium Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+class Problem_1760A
+{
+    static void Main(string[] args)
+    {
+        var count = int.Parse(ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    var numbers = ReadLine().Split().Select(int.Parse).ToArray();
+    Array.Sort(numbers);
+    WriteLine(numbers[1]);
+}
+    }
+}
