f502032a5b5f9f9a25c9fd2e0e0d6824441cc53d
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2023-07-07 11:03:57 +0600

    solved

---
+++ CodeForces/Codes/151A - Soft Drinking/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+class Program_151A
+{
+    static void Main()
+    {
+        int[] ingredients = Array.ConvertAll(ReadLine().Split(), int.Parse);
+
+        int[] element = {((ingredients[1]*ingredients[2])/ingredients[6])/ingredients[0],(ingredients[3]*ingredients[4])/ingredients[0],(ingredients[5]/ingredients[7])/ingredients[0]};
+       int temp = element[0];
+       foreach(int entry in element)
+       {
+           if(entry<temp)
+           temp = entry;
+           else
+           continue;
+       }
+        WriteLine($"{temp}");
+    }
+}
