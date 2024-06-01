b83895a961e21625bc7d4864a215fb99982854dd
MD. SHAKIBUL HOSSAIN SIKDER <89571450+Sikder-SWE@users.noreply.github.com>
2024-06-01 20:06:08 +0600

    SOlved

---
+++ CodeForces/Codes/1409A- Yet Another Two integers Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+int testCase = int.Parse(ReadLine());
+
+while (testCase > 0)
+{
+    var ab = Array.ConvertAll(ReadLine().Split(), int.Parse);
+    int difference = Math.Abs(ab[0] - ab[1]);
+    Console.WriteLine(difference/10+(difference%10>0?1:0));
+    testCase--;
+}
